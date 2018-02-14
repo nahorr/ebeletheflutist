9fb2e08e36a1c3987e3055114c6788e9e9b9dc2d
Nnamdi Okeke <nahorr@gmail.com>
2018-02-14 14:39:35 -0700

    home public

--- resources/views/homepublic/layouts/header.blade.php
+++ resources/views/homepublic/layouts/header.blade.php
@@ -5,52 +5,13 @@
 					<!-- Contact starts -->
 					<div class="tb-contact pull-left">
 						<!-- Email -->
-						<i class="fa fa-envelope color"></i> &nbsp; <a href="mailto:contact@domain.com">nnamdi@nahorr.com</a>
+						<i class="fa fa-envelope color"></i> &nbsp; <a href="mailto:contact@domain.com">ebele@ebeletheflutist.com</a>
 						&nbsp;&nbsp;  
 						<!-- Phone -->
-						<i class="fa fa-phone color"></i> &nbsp; +1 (403)-(402)-2387
+						<i class="fa fa-phone color"></i> &nbsp; +234 803 710 4996
 					</div>
 					<!-- Contact ends -->
 					
-					<!-- Shopping kart starts -->
-					<div class="tb-shopping-cart pull-right">
-						<!-- Link with badge -->
-						<a href="#" class="btn btn-white btn-xs b-dropdown"><i class="fa fa-shopping-cart"></i> <i class="fa fa-angle-down color"></i> <span class="badge badge-color">5</span></a>
-						<!-- Dropdown content with item details -->
-						<div class="b-dropdown-block">
-							<!-- Heading -->
-							<h4><i class="fa fa-shopping-cart color"></i> Your Items</h4>
-							<ul class="list-unstyled">
-								<!-- Item 1 -->
-								<li>
-									<!-- Item image -->
-									<div class="cart-img">
-									<a href="#"><img src="{{asset('brave/img/ecommerce/view-cart/1.png')}}" alt="" class="img-responsive" /></a>
-									</div>
-									<!-- Item heading and price -->
-									<div class="cart-title">
-										<h5><a href="#">Samsamgun galaxy some thing about.</a></h5>
-										<!-- Item price -->
-										<span class="label label-color label-sm">$1,900</span>
-									</div>
-									<div class="clearfix"></div>
-								</li>
-								<!-- Item 2 -->
-								<li>
-									<div class="cart-img">
-									<a href="#"><img src="{{asset('brave/img/ecommerce/view-cart/2.png')}}" alt="" class="img-responsive" /></a>
-									</div>
-									<div class="cart-title">
-										<h5><a href="#">Samsamgun galaxy some thing about.</a></h5>
-										<span class="label label-color label-sm">$1,900</span>
-									</div>
-									<div class="clearfix"></div>
-								</li>
-							</ul>
-							<a href="#" class="btn btn-white btn-sm">View Cart</a> &nbsp; <a href="#" class="btn btn-color btn-sm">Checkout</a>
-						</div>
-					</div>
-					<!-- Shopping kart ends -->
 					
 					<!-- Langauge starts -->
 					<div class="tb-language dropdown pull-right">
@@ -59,32 +20,16 @@
 					</div>
 					<!-- Language ends -->
 					
-					<!-- Search section for responsive design -->
-					<div class="tb-search pull-left">
-						<a href="#" class="b-dropdown"><i class="fa fa-search square-2 rounded-1 bg-color white"></i></a>
-						<div class="b-dropdown-block">
-							<form role="form">
-								<!-- Input Group -->
-								<div class="input-group">
-									<input type="text" class="form-control" placeholder="Type Something">
-									<span class="input-group-btn">
-										<button class="btn btn-color" type="button">Search</button>
-									</span>
-								</div>
-							</form>
-						</div>
-					</div>
-					<!-- Search section ends -->
-					
+									
 					<!-- Social media starts -->
 					<div class="tb-social pull-right">
 						<div class="brand-bg text-right">
 							<!-- Brand Icons -->
-							<a href="#" class="facebook"><i class="fa fa-facebook square-2 rounded-1"></i></a>
-							<a href="#" class="twitter"><i class="fa fa-twitter square-2 rounded-1"></i></a>
-							<a href="#" class="google-plus"><i class="fa fa-google-plus square-2 rounded-1"></i></a>
-							<a href="#" class="linkedin"><i class="fa fa-linkedin square-2 rounded-1"></i></a>
-							<a href="#" class="pinterest"><i class="fa fa-pinterest square-2 rounded-1"></i></a>
+							<a href="https://free.facebook.com/NigForemostFemaleFlutist/?_rdc=1&_rdr" class="facebook" target="_blank"><i class="fa fa-facebook square-2 rounded-1"></i></a>
+							<a href="https://twitter.com/ebeletheflutist" class="twitter" target="_blank"><i class="fa fa-twitter square-2 rounded-1"></i></a>
+							<a href="https://plus.google.com/109824567381646809495" class="google-plus" target="_blank"><i class="fa fa-google-plus square-2 rounded-1"></i></a>
+							<a href="https://ng.linkedin.com/in/ebele-the-flutist-43a60555" class="linkedin" target="_blank"><i class="fa fa-linkedin square-2 rounded-1"></i></a>
+							<a href="https://www.instagram.com/explore/tags/ebeletheflutist/" class="pinterest" target="_blank"><i class="fa fa-instagram square-2 rounded-1"></i></a>
 						</div>
 					</div>
 					<!-- Social media ends -->
@@ -101,58 +46,26 @@
 				<!-- Container -->
 				<div class="container">
 					<div class="row">
-						<div class="col-md-2">
+						<div class="col-md-4">
 							<!-- Logo section -->
 							<div class="logo">
-								<h1><a href="#"><i class="fa fa-laptop"></i> Nahorr</a></h1>
+								<a href="href="{{ url('/') }}">
+									<img src="{{asset('brave/img/ebeletheflutist_logo.png')}}" alt="ebeletheflutist" />
+								</a>
 							</div>
+							
 						</div>
-						<div class="col-md-9">
+						<div class="col-md-7">
 						
 							<!-- Navigation starts.  -->
 							<div class="navy">			
 								<ul>
 									<!-- Main menu -->
-									<li><a href="#">Home</a>
-										<!-- Submenu -->
-										<ul>
-											<li><a href="sitemap.html" class="red">Complete List</a></li>
-											<li><a href="index.html">Main Home</a></li>
-											<li><a href="index-bootstrap-carousel.html">Home Carousel</a></li>
-											<li><a href="index-magazine.html">Home Magazine</a></li>
-											<!-- 2nd Level sub menu -->
-											<li>
-												<a href="#">More #1</a>
-												<ul>
-													<li><a href="blog-masonry.html">Masonry Style</a></li>
-													<li><a href="medical-index.html">Medical</a></li>
-													<li><a href="ecommerce-index.html">Ecommerce</a></li>
-													<li><a href="non-profit-index.html">Non Profit</a></li>
-													<li><a href="coming-soon-image.html">Coming Soon</a></li>
-													<li><a href="index-slim.html">Home Slim</a></li>
-													<li><a href="index-sidebar.html">Home Sidebar</a></li>
-													<li><a href="index-parallax-slider.html">Home Parallax Slider</a></li>
-												</ul>
-											</li>
-											<li>
-												<a href="#">More #2</a>
-												<ul>
-													<li><a href="real-estate-index.html">Real Estate</a></li>
-													<li><a href="restaurant-index.html">Restaurant</a></li>
-													<li><a href="resume.html">Resume</a></li>
-													<li><a href="v-card-image.html">V Card</a></li>
-													<li><a href="wedding.html">Wedding</a></li>
-													<li><a href="index-parallax.html">Parallax Home</a></li>
-													<li><a href="header-2.html">Header Alt</a></li>
-													<li><a href="index-portfolio.html">Home Portfolio</a></li>
-												</ul>
-											</li>
-										</ul>
-									</li>
+									<li><a href="{{ url('/') }}">HOME</a></li>
 								
-									<li><a href="#">Components</a>
+									<li><a href="#">MUSIC</a>
 										<ul>
-											<li><a href="#">Components #1</a>
+											<li><a href="#">Audios </a>
 												<ul>
 													<li><a href="accordion.html">Accordion</a></li>
 													<li><a href="blockquote.html">Blockquote</a></li>
@@ -164,7 +77,7 @@
 													<li><a href="cta.html">CTA</a></li>
 												</ul>
 											</li>
-											<li><a href="#">Components #2</a>
+											<li><a href="#">Videos </a>
 												<ul>
 													<li><a href="divider.html">Divider</a></li>
 													<li><a href="bootstrap-carousel.html">Carousel</a></li>
@@ -176,7 +89,7 @@
 													<li><a href="knob.html">Knob</a></li>
 												</ul>
 											</li>
-											<li><a href="#">Components #3</a>
+											<li><a href="#">Lyrics</a>
 												<ul>
 													<li><a href="nav-tabs.html">Nav Tabs</a></li>
 													<li><a href="owl-carousel.html">Owl Carousel</a></li>
@@ -188,150 +101,40 @@
 													<li><a href="testimonial.html">Testimonial</a></li>
 												</ul>
 											</li>
-											<!-- Multi level menu -->
-											<li><a href="#">Multi Level Menu</a>
+											<li><a href="#">Music/Flute Training</a>
 												<ul>
-													<!-- Sub menu -->
-													<li><a href="#">Menu #1</a></li>
-													<li><a href="#">Menu #1</a></li>
-													<li><a href="#">Menu #1</a>
-														<ul>
-															<!-- Sub menu -->
-															<li><a href="#">Menu #2</a></li>
-															<li><a href="#">Menu #2</a></li>
-															<li><a href="#">Menu #2</a>
-																<ul>
-																	<!-- Sub menu -->
-																	<li><a href="#">Menu #3</a></li>
-																	<li><a href="#">Menu #3</a></li>
-																	<li><a href="#">Menu #3</a></li>
-																</ul>
-															</li>
-														</ul>
-													</li>
+													<li><a href="nav-tabs.html">Nav Tabs</a></li>
+													<li><a href="owl-carousel.html">Owl Carousel</a></li>
+													<li><a href="panel.html">Panel</a></li>
+													<li><a href="pricing-table.html">Pricing Table</a></li>
+													<li><a href="progress-bar.html">Progress Bar</a></li>
+													<li><a href="tables.html">Tables</a></li>
+													<li><a href="team.html">Team</a></li>
+													<li><a href="testimonial.html">Testimonial</a></li>
 												</ul>
 											</li>
+											
 										</ul>
 									</li>
 									
-									<li><a href="#">Blog</a>
-										<ul>
-											<li><a href="blog-one.html">Blog One</a></li>
-											<li><a href="blog-two.html">Blog Two</a></li>
-											<li><a href="blog-three.html">Blog Three</a></li>
-											<li><a href="blog-four.html">Blog Four</a></li>
-											<li><a href="blog-single.html">Blog Single</a></li>
-											<li><a href="blog-masonry.html">Blog Masonry</a></li>
-										</ul>
-									</li>
-									
-									<li><a href="#">Themes</a>
-										<ul>
-											<li><a href="ui-designs.html">UI Designs</a></li>
-											<li><a href="#">Ecommerce</a>
-												<!-- Sub dropdown menu -->
-												<ul>
-													<li><a href="ecommerce-index.html">Home</a></li>
-													<li><a href="ecommerce-account.html">Account</a></li>
-													<li><a href="ecommerce-checkout.html">Checkout</a></li>
-													<li><a href="ecommerce-items.html">Items</a></li>
-													<li><a href="ecommerce-item-single.html">Items Single</a></li>
-													<li><a href="ecommerce-items-ui.html">Items UI</a></li>
-													<li><a href="ecommerce-order-history.html">Order History</a></li>
-													<li><a href="ecommerce-view-cart.html">View Cart</a></li>
-													<li><a href="ecommerce-wish-list.html">Wish List</a></li>
-												</ul>
-											</li>
-											<li><a href="#">Medical</a>
-												<!-- Sub dropdown menu -->
-												<ul>
-													<li><a href="medical-index.html">Home</a></li>
-													<li><a href="medical-appointment.html">Appointment</a></li>
-													<li><a href="medical-department.html">Department</a></li>
-													<li><a href="medical-our-doctor.html">Our Doctor</a></li>
-													<li><a href="medical-treatment.html">Treatment</a></li>
-												</ul>
-											</li>
-											<li><a href="#">Real Estate</a>
-												<!-- Sub dropdown menu -->
-												<ul>
-													<li><a href="real-estate-index.html">Home</a></li>
-													<li><a href="real-estate-agencies.html">Agencies</a></li>
-													<li><a href="real-estate-agents.html">Agents</a></li>
-													<li><a href="real-estate-enquiry.html">Enquiry</a></li>
-													<li><a href="real-estate-property-map.html">Property Map</a></li>
-													<li><a href="real-estate-property-single.html">Property Single</a></li>
-												</ul>
-											</li>
-											<li><a href="#">Restaurant</a>
-												<!-- Sub dropdown menu -->
-												<ul>
-													<li><a href="restaurant-index.html">Home</a></li>
-													<li><a href="restaurant-checkout.html">Checkout</a></li>
-													<li><a href="restaurant-items.html">Items</a></li>
-													<li><a href="restaurant-item-single.html">Items Single</a></li>
-													<li><a href="restaurant-menu.html">Menu</a></li>
-													<li><a href="restaurant-nutrition-info.html">Nutrition Info</a></li>
-													<li><a href="restaurant-recipe.html">Recipe</a></li>
-													<li><a href="restaurant-reserve-seats.html">Reserve Seats</a></li>
-												</ul>
-											</li>
-											<li><a href="#">Non Profit</a>
-												<!-- Sub dropdown menu -->
-												<ul>
-													<li><a href="non-profit-index.html">Home</a></li>
-													<li><a href="non-profit-action.html">Action</a></li>
-													<li><a href="non-profit-action-single.html">Action Single</a></li>
-													<li><a href="non-profit-causes.html">Causes</a></li>
-													<li><a href="non-profit-donation.html">Donation</a></li>
-													<li><a href="non-profit-events.html">Events</a></li>
-													<li><a href="non-profit-goal.html">Goal</a></li>
-													<li><a href="non-profit-volunteers.html">Volunteers</a></li>
-												</ul>
-											</li>
-										</ul>
-									</li>
-									
-									<li><a href="#">Portfolio</a>
-										<ul>
-											<li><a href="portfolio-one.html">Portfolio One</a></li>
-											<li><a href="portfolio-two.html">Portfolio Two</a></li>
-											<li><a href="portfolio-three.html">Portfolio Three</a></li>
-											<li><a href="portfolio-four.html">Portfolio Four</a></li>
-											<li><a href="portfolio-single.html">Portfolio Single</a></li>
-										</ul>
-									</li>	
-
-									<li><a href="about-us-one.html">About Us</a></li>
-									<li><a href="contact-us-one.html">Contact Us</a></li>
+									<li><a href="about-us-one.html">ABOUT EBELE</a></li>
+									<li><a href="contact-us-one.html">CONTACT US</a></li>
 								</ul>
 							</div>							
 							<!-- Navigation ends -->
 							
 						</div>
-						
+
 						<div class="col-md-1">
 							
 							<!-- Search section -->
 							<div class="head-search pull-right">
-								<a href="#" class="b-dropdown"><i class="fa fa-search square-2 rounded-1 bg-color white"></i></a>
-								<div class="b-dropdown-block">
-									<form role="form">
-										<!-- Input Group -->
-										<div class="input-group">
-											<input type="text" class="form-control" placeholder="Type Something">
-											<span class="input-group-btn">
-												<button class="btn btn-color" type="button">Search</button>
-											</span>
-										</div>
-									</form>
-								</div>
+								<button class="btn btn-warning" type="button"><a href="https://nahorr.com/"><strong>EBELE'S WORLD</strong></a></button>
 							</div>
-							<!-- Search section ends -->
-							<div class="clearfix"></div>
-							
 						</div>
-						
+						<!-- Search section ends -->
+						<div class="clearfix"></div>
+												
 					</div>
 				</div>
 			</div>
