a54ce33fa11a81cc9271b4d3ad8fa9d98c719d2c
Nnamdi Okeke <nahorr@gmail.com>
2018-03-02 14:59:18 -0700

    post tip

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -5,7 +5,7 @@
 
                 <ul class="nav nav-list">
                     <li class="active">
-                        <a href="index.html">
+                        <a href="{{url('admin/home')}}">
                             <i class="menu-icon fa fa-tachometer"></i>
                             <span class="menu-text"> Dashboard </span>
                         </a>
