1d156865569abf4e25047d49c519128f92f7f646
Nnamdi Okeke <nahorr@gmail.com>
2018-03-05 08:21:56 -0700

    fix tips loop

--- resources/views/homepublic/musicflutetrainingtipsbyebele.blade.php
+++ resources/views/homepublic/musicflutetrainingtipsbyebele.blade.php
@@ -28,21 +28,25 @@
 					<div class="container">
 						<div class="row">
 							@foreach($tips as $tip)
-							<div class="col-md-4">
-								<!-- Image Box #6 Item -->
-								<div class="img-box-6-item">
-									<!-- Image Box #6 Image -->
-									<div class="img-box-6-img">
-										<!-- Image -->
-										<a href="{{url('/musicflutetrainingtipsbyebeletip/'.$tip->id)}}" ><img src="{{asset('/tips/images/'.$tip->tip_image )}}" alt="" class="img-responsive" /></a>
-									</div>	
-									<!-- Image Box #6 Content -->
-									<div class="img-box-6-content">
-										<h4><a href="{{url('/musicflutetrainingtipsbyebeletip/'.$tip->id)}}">{{$tip->tip_title}}</a></h4>
-										
+								
+									<div class="col-md-4">
+										<!-- Image Box #6 Item -->
+										<div class="img-box-6-item">
+											<!-- Image Box #6 Image -->
+											<div class="img-box-6-img">
+												<!-- Image -->
+												<a href="{{url('/musicflutetrainingtipsbyebeletip/'.$tip->id)}}" ><img src="{{asset('/tips/images/'.$tip->tip_image )}}" alt="" class="img-responsive" /></a>
+											</div>	
+											<!-- Image Box #6 Content -->
+											<div class="img-box-6-content">
+												<h4><a href="{{url('/musicflutetrainingtipsbyebeletip/'.$tip->id)}}">{{$tip->tip_title}}</a></h4>
+												
+											</div>
+										</div>
 									</div>
-								</div>
-							</div>
+								@if($loop->count == 4)
+									@break
+								@endif
 							@endforeach
 						</div>
 					</div>
