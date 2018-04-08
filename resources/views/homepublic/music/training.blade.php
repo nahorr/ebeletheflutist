edfe11a531d465b91e22c6c1a54b329833dc4411
nahorr <nahorr@gmail.com>
2018-04-08 12:52:45 -0600

    training videos

--- resources/views/homepublic/music/training.blade.php
+++ resources/views/homepublic/music/training.blade.php
@@ -22,6 +22,36 @@
 				<div class="container">
 					
 					<div class="row">
+
+						<div class="col-md-6 col-sm-6">
+							
+							<div class="image-style-seven">
+								<!-- Image -->
+								
+								<iframe  width="100%" height="315" src="https://www.youtube.com/embed/YCSRSe6nPY8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+								
+							</div>
+							
+							<!-- Heading -->
+							<div class="block-heading-two">
+								<h3><span>Intro Video</span></h3>
+							</div>
+						
+						</div>
+						<div class="col-md-6 col-sm-6">
+							
+							<div class="image-style-seven">
+								<!-- Image -->
+								<iframe  width="100%" height="315" src="https://ebeletheflutist.com/training/videos/rudiment_of_music.mp4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+								
+							</div>
+							
+							<!-- Heading -->
+							<div class="block-heading-two">
+								<h3><span>Rudiments Of Music part 1</span></h3>
+							</div>
+						
+						</div>
 					@foreach($videos as $video)
 					
 						<div class="col-md-6 col-sm-6">
